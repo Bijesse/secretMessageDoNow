9d485b050b7f3d8537130bdd06a9ff81783ce946
Tom Bijesse <tom@bijesse.com>
2016-07-15 10:19:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-$( document ).ready(function() {
-
     // declaring the secret message
     var secretMessage = "ScriptEd Rules!";
 
     // prints the secret message to the console
     console.log( secretMessage );
-});
+
